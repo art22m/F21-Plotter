df3d47ab7cc8f0d6267d73ba6a272d6208894496
Artem <artmurashko@yandex.ru>
2021-10-11 22:01:27 +0300

    Initial Commit

---
+++ ComputationalPracticum/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ComputationalPracticum
+//
+//  Created by Артём Мурашко on 11.10.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
